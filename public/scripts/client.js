ac75653e201b84857ceb1ce7bf4038ccc8221fb2
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-27 13:19:10 -0400

    add to function createTweetElement & renderTweets empty ()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,3 +6,47 @@
  */
 
 
+// Create tweet HTML element from a tweet object
+const createTweetElement = function(tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="tweet-author">
+          <img src="${tweet.user.avatars}" alt="Profile picture">
+          <div class="tweet-info">
+            <h2 class="author-name">${tweet.user.name}</h2>
+            <p class="author-handle">${tweet.user.handle}</p>
+          </div>
+        </div>
+        <div class="tweet-time">
+          <time datetime="${tweet.created_at}">${timeago.format(tweet.created_at)}</time>
+        </div>
+      </header>
+      <div class="tweet-content">
+        <p>${escape(tweet.content.text)}</p>
+      </div>
+      <footer>
+        <div class="tweet-actions">
+          <a href="#" class="like-action"><i class="fas fa-heart"></i> </a>
+          <a href="#" class="retweet-action"><i class="fas fa-retweet"></i> </a>
+          <a href="#" class="flag-action"><i class="fas fa-flag"></i></a>
+        </div>
+      </footer>
+    </article>
+  `);
+
+  return $tweet;
+};
+
+// function to render tweets from an array of tweet data
+const renderTweets = function(tweets) {
+  const $tweetsContainer = $('#tweets-container');
+  
+  $tweetsContainer.empty(); // clear the container first
+  
+  tweets.forEach(tweet => {
+    const $tweet = createTweetElement(tweet);
+    $tweetsContainer.append($tweet);
+  });
+};
+  
