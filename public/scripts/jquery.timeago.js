2e8a8a09b54b9ccf23ae619a447aac77812ccd83
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-28 05:02:23 -0400

    update date format

--- public/scripts/jquery.timeago.js
+++ public/scripts/jquery.timeago.js
@@ -3,22 +3,13 @@
 
 // Wait for the document to be ready before running any jQuery code
 $(document).ready(function() {
-  // Find all elements with class "timeago" and apply the timeago function
   $('time.timeago').timeago();
-  });
-  
-  // Get the timestamp of the tweet
-  const tweetTimestamp = Date.now() - 1000 * 60 * 5; // Example: 5 minutes ago
-  
-  // Convert the timestamp to a human-readable format using the timeago library
-  const timeAgo = $.timeago(tweetTimestamp);
-  console.log(timeAgo); // Output: "5 minutes ago"
-  
-  // Display the time passed since the tweet
+
+  const tweetDate = new Date(Date.now() - 1000 * 60 * 5);
+  const timeAgo = $.timeago(tweetDate);
+
   const tweetTimeElement = document.querySelector('.tweet-time');
   tweetTimeElement.textContent = timeAgo;
-  
-  // Assume date is a Date object representing the time the tweet was posted
-  const tweetDate = new Date(Date.now() - 1000 * 60 * 5); // Example: 5 minutes ago
-  const timeAgo = $.timeago(tweetDate);
-  console.log(timeAgo); // Output: "5 minutes ago"
+
+  console.log(timeAgo);
+});
