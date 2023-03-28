7426572788877c6f98dee5c2be36e294b382991f
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-28 04:57:41 -0400

    update date format

--- public/scripts/jquery.timeago.js
+++ public/scripts/jquery.timeago.js
@@ -1,26 +1,24 @@
 /* jquery.timeago.js */
 
-// Get the timestamp of the tweet
-const tweetTimestamp = Date.now() - 1000 * 60 * 5; // Example: 5 minutes ago
 
-// Convert the timestamp to a human-readable format using the timeago library
-const time-Ago = jQuery.timeago(tweetTimestamp);
-console.log(time-Ago); // Output: "5 minutes ago"
-
-// Display the time passed since the tweet
-const tweetTimeElement = document.querySelector('.tweet-time');
-tweetTimeElement.textContent = time-Ago;
-
-
-// Assume `date` is a Date object representing the time the tweet was posted
-var timeAgo = jQuery.timeago(date);
-// `timeAgo` will be a string like "5 minutes ago"
-
-
-const tweetDate = new Date(Date.now() - 1000 * 60 * 5); // Example: 5 minutes ago
-const time-Ago = jQuery.timeago(tweetDate);
-console.log(time-Ago); // Output: "5 minutes ago"
-
-
-
-
+// Wait for the document to be ready before running any jQuery code
+$(document).ready(function() {
+  // Find all elements with class "timeago" and apply the timeago function
+  $('time.timeago').timeago();
+  });
+  
+  // Get the timestamp of the tweet
+  const tweetTimestamp = Date.now() - 1000 * 60 * 5; // Example: 5 minutes ago
+  
+  // Convert the timestamp to a human-readable format using the timeago library
+  const timeAgo = $.timeago(tweetTimestamp);
+  console.log(timeAgo); // Output: "5 minutes ago"
+  
+  // Display the time passed since the tweet
+  const tweetTimeElement = document.querySelector('.tweet-time');
+  tweetTimeElement.textContent = timeAgo;
+  
+  // Assume date is a Date object representing the time the tweet was posted
+  const tweetDate = new Date(Date.now() - 1000 * 60 * 5); // Example: 5 minutes ago
+  const timeAgo = $.timeago(tweetDate);
+  console.log(timeAgo); // Output: "5 minutes ago"
