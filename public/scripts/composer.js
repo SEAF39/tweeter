34a1644762150ecfb5bf8465fe5b39691a3b42b4
SEAF39 <labber@DESKTOP-7J1C764>
2023-04-04 02:45:17 -0400

    create the scroll up code

---
+++ public/scripts/composer.js
@@ -0,0 +1,21 @@
+
+// Stretch: Scroll to-top button
+const topScroll = function() {
+  $(document).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+      $("div.top-up")
+      .show(300)
+      .on("click", function() {
+        $(document).off("scroll");
+        $("html, body").stop(true, false).animate({ scrollTop: "0" }, 300, () => {
+          $("div.top-up").hide(300);
+          $(document).scroll(topScroll);
+        });
+      });
+    }
+  });
+};
+
+
+
+ $("div.top-up").hide();
